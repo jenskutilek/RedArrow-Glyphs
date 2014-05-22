29b51ca85a46ba66046ea18b7705e38ab10748a0
Jens Kutilek <webmail@kutilek.de>
2014-05-22 18:19:56 +0200

    Differentiate between hor. and vert. extrema

--- RedArrow.glyphsReporter/Contents/Resources/helpers.py
+++ RedArrow.glyphsReporter/Contents/Resources/helpers.py
@@ -2,14 +2,19 @@ from miniFontTools.bezierTools import calcCubicParameters, solveQuadratic, split
 
 # Helper functions
 
-def getExtremaForCubic(pt1, pt2, pt3, pt4):
+def getExtremaForCubic(pt1, pt2, pt3, pt4, h=True, v=False):
     (ax, ay), (bx, by), c, d = calcCubicParameters(pt1, pt2, pt3, pt4)
     ax *= 3.0
     ay *= 3.0
     bx *= 2.0
     by *= 2.0
-    roots  = [t for t in solveQuadratic(ax, bx, c[0]) if 0 < t < 1]
-    roots += [t for t in solveQuadratic(ay, by, c[1]) if 0 < t < 1]
+    h_roots = []
+    v_roots = []
+    if h:
+        h_roots = [t for t in solveQuadratic(ay, by, c[1]) if 0 < t < 1]
+    if v:
+        v_roots  = [t for t in solveQuadratic(ax, bx, c[0]) if 0 < t < 1]
+    roots = h_roots + v_roots
     return [p[3] for p in splitCubicAtT(pt1, pt2, pt3, pt4, *roots)[:-1]]
 
 
