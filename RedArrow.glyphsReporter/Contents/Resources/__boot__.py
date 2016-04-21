d38f6b4ff4ce86254596da5d15127248d5ebba48
Jens Kutilek <webmail@kutilek.de>
2016-04-21 14:23:19 +0200

    Add some (inactive) debug code

--- RedArrow.glyphsReporter/Contents/Resources/__boot__.py
+++ RedArrow.glyphsReporter/Contents/Resources/__boot__.py
@@ -1,5 +1,6 @@
-import GlyphsApp
+import GlyphsApp #, traceback
 from GlyphsApp import Glyphs
+#from Foundation import NSLog
 
 def _run(*scripts):
 	global __file__
@@ -10,8 +11,12 @@ def _run(*scripts):
 		path = os.path.join(base, script)
 		__file__ = path
 		execfile(path, globals(), globals())
-
+		
+#NSLog("Test___________")
+#try:
 if hasattr(Glyphs, 'versionNumber') and Glyphs.versionNumber >= 2.3:
 	_run('RedArrow.py')
 else:
 	_run('RedArrow22.py')
+#except:
+#	NSLog(traceback.format_exc())
