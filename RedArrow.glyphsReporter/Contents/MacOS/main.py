22347fe7c0dec2392008fb3857370ed1d6e9b18f
schriftgestalt <georg.seifert@mac.com>
2017-04-09 22:32:47 +0200

    use newer plugin structure

--- RedArrow.glyphsReporter/Contents/MacOS/main.py
+++ RedArrow.glyphsReporter/Contents/MacOS/main.py
@@ -1,15 +1,11 @@
-import GlyphsApp #, traceback
-from GlyphsApp import Glyphs
-#from Foundation import NSLog
-
 def _run(*scripts):
 	global __file__
-	import os, sys
+	import os, sys #, site
 	sys.frozen = 'macosx_plugin'
 	base = os.environ['RESOURCEPATH']
 	for script in scripts:
 		path = os.path.join(base, script)
 		__file__ = path
 		execfile(path, globals(), globals())
-		
-_run('RedArrow.py')
+
+_run('plugin.py')
