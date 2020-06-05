d1da136dea5eca7648e2ad6f4858cfee970ea524
Jens Kutilek <webmail@kutilek.de>
2020-06-05 11:03:29 +0200

    Update FontTools

--- RedArrow.glyphsReporter/Contents/Resources/miniFontTools/pens/__init__.py
+++ RedArrow.glyphsReporter/Contents/Resources/miniFontTools/pens/__init__.py
@@ -1,3 +1,3 @@
-"""Empty __init__.py file to signal Python this directory is a package.
-(It can't be completely empty since WinZip seems to skip empty files.)
-"""
+"""Empty __init__.py file to signal Python this directory is a package."""
+
+from fontTools.misc.py23 import *
